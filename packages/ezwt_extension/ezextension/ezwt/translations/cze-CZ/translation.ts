5b9710b88fc9cbd718793e7e8bc8fc710e1bb951
Petr Mifek <petr.mifek@anapol.cz>
2011-12-30 22:30:08 +0100

    Completed and fixed cze-CZ translation for ezwt.

--- packages/ezwt_extension/ezextension/ezwt/translations/cze-CZ/translation.ts
+++ packages/ezwt_extension/ezextension/ezwt/translations/cze-CZ/translation.ts
@@ -1,26 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="cs_CZ">
 <context>
     <name>design/admin/node/view/full</name>
     <message>
         <source>Remove selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Odebrat vybrané</translation>
     </message>
 </context>
 <context>
     <name>design/standard/content/edit</name>
     <message>
         <source>Send for publishing</source>
-        <translation>Oeslat pro publikování</translation>
+        <translation>Odeslat k publikaci</translation>
     </message>
     <message>
         <source>Manage versions</source>
         <translation>Spravovat verze</translation>
-    </message>
-    <message>
-        <source>Store and exit</source>
-        <translation type="obsolete">Uložit a odejít</translation>
     </message>
     <message>
         <source>Preview</source>
@@ -36,46 +32,42 @@
     </message>
     <message>
         <source>Publish the contents of the draft that is being edited. The draft will become the published version of the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Publikovat koncept který je upravován.</translation>
     </message>
     <message>
         <source>Store draft</source>
-        <translation type="unfinished"></translation>
+        <translation>Uložit koncept</translation>
     </message>
     <message>
         <source>Store the contents of the draft that is being edited and continue editing. Use this button to periodically save your work while editing.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uloží koncept. Použijte toto tlačítko pro periodické ukládání vaší práce do eZ Publish během větších prací na obsahu.</translation>
     </message>
     <message>
         <source>Store draft and exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Uložit koncept a odejít</translation>
     </message>
     <message>
         <source>Store the draft that is being edited and exit from edit mode. Use when you need to exit your work and return later to continue.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uloží koncept a ukončí editaci. Použijte pro uložení rozpracovaného konceptu k pozdějšímu dokončení.</translation>
     </message>
     <message>
         <source>Discard draft</source>
-        <translation type="unfinished"></translation>
+        <translation>Zahodit koncept</translation>
     </message>
     <message>
         <source>Are you sure you want to discard the draft?</source>
-        <translation type="unfinished"></translation>
+        <translation>Opravdu chcete zahodit tento koncept? Změny v něm provedené budou ztraceny.</translation>
     </message>
     <message>
         <source>Discard the draft that is being edited. This will also remove the translations that belong to the draft (if any).</source>
-        <translation type="unfinished"></translation>
+        <translation>Zahodit koncept který je upravován. Tato akce také zahodí případné překlady tohoto konceptu, pokud existují.</translation>
     </message>
 </context>
 <context>
     <name>design/standard/content/view/versionview</name>
     <message>
-        <source>Manage versions</source>
-        <translation type="obsolete">Spravovat verze</translation>
-    </message>
-    <message>
         <source>Edit</source>
-        <translation>Editovat</translation>
+        <translation>Upravit</translation>
     </message>
     <message>
         <source>Publish</source>
@@ -89,10 +81,6 @@
         <translation>Vytvořit zde</translation>
     </message>
     <message>
-        <source>Edit: %node_name [%class_name]</source>
-        <translation type="obsolete">Editovat %node_name [%class_name]</translation>
-    </message>
-    <message>
         <source>Move</source>
         <translation>Přesunout</translation>
     </message>
@@ -102,11 +90,11 @@
     </message>
     <message>
         <source>Add locations</source>
-        <translation>Přidat lokace</translation>
+        <translation>Přidat umístění</translation>
     </message>
     <message>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Třídění</translation>
     </message>
     <message>
         <source>Replace</source>
@@ -130,42 +118,42 @@
     </message>
     <message>
         <source>Edit object states</source>
-        <translation type="unfinished"></translation>
+        <translation>Upravit stavy objektu</translation>
     </message>
     <message>
         <source>Edit</source>
-        <translation type="unfinished">Editovat</translation>
+        <translation>Upravit</translation>
     </message>
     <message>
         <source>Go to admin interface.</source>
-        <translation type="unfinished"></translation>
+        <translation>Přejít do administračního rozhraní.</translation>
     </message>
     <message>
         <source>Toggle menu link type edit.</source>
-        <translation type="unfinished"></translation>
+        <translation>Přepnout editaci odkazů v menu.</translation>
     </message>
 </context>
 <context>
     <name>design/standard/websitetoolbar/sort</name>
     <message>
         <source>Sub items [%children_count]</source>
-        <translation type="unfinished"></translation>
+        <translation>Podprvky [%children_count]</translation>
     </message>
     <message>
         <source>Show 10 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukázat 10 prvků na stranu.</translation>
     </message>
     <message>
         <source>Show 50 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukázat 50 prvků na stranu.</translation>
     </message>
     <message>
         <source>Show 25 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukázat 25 prvků na stranu.</translation>
     </message>
     <message>
         <source>Priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Priorita</translation>
     </message>
     <message>
         <source>Name</source>
@@ -185,47 +173,43 @@
     </message>
     <message>
         <source>Use the priority fields to control the order in which the items appear. You can use both positive and negative integers. Click the &quot;Update priorities&quot; button to apply the changes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Použijte políčka priorit ke přímé kontrole pořadí, v jakém budou prvky zobrazeny. Můžete použít kladná i záporná čisla. Klikněte na tlačítko &quot;Uložit priority&quot; pro uložení změn.</translation>
     </message>
     <message>
         <source>You are not allowed to update the priorities because you do not have permission to edit &lt;%node_name&gt;.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Unknown</source>
-        <translation type="obsolete">Neznámý</translation>
+        <translation>Nemáte oprávnění k úpravě priorit protože nemáte oprávnění ke změnám &lt;%node_name&gt;.</translation>
     </message>
     <message>
         <source>The current item does not contain any sub items.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuální prven nemá žádné podprvky.</translation>
     </message>
     <message>
         <source>Update priorities</source>
-        <translation type="unfinished"></translation>
+        <translation>Uložit priority</translation>
     </message>
     <message>
         <source>Apply changes to the priorities of the items in the list above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplikovat změny priorit provedené v seznamu nahoře.</translation>
     </message>
     <message>
         <source>You cannot update the priorities because you do not have permission to edit the current item or because a non-priority sorting method is used.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nemůžete změnit priority protože nemáte oprávnění k úpravám aktuálního prvku, nebo není nastaveno třídění podle priorit.</translation>
     </message>
     <message>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Třídění</translation>
     </message>
     <message>
         <source>Class identifier</source>
-        <translation type="unfinished"></translation>
+        <translation>Identifikátor typu</translation>
     </message>
     <message>
         <source>Class name</source>
-        <translation type="unfinished"></translation>
+        <translation>Název typu</translation>
     </message>
     <message>
         <source>Depth</source>
-        <translation type="unfinished"></translation>
+        <translation>Hloubka</translation>
     </message>
     <message>
         <source>Published</source>
@@ -233,11 +217,11 @@
     </message>
     <message>
         <source>You cannot set the sorting method for the current location because you do not have permission to edit the current item.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nemůžete nastavit metodu třídění aktuálního umístění protože nemáte oprávnění k úpravám aktuálního prvku.</translation>
     </message>
     <message>
         <source>Use these controls to set the sorting method for the sub items of the current location.</source>
-        <translation type="unfinished"></translation>
+        <translation>Použijte tyto prvky pro nastavení metody třídění podprvků aktuální lokace.</translation>
     </message>
     <message>
         <source>Descending</source>
@@ -249,35 +233,35 @@
     </message>
     <message>
         <source>Set</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavit</translation>
     </message>
     <message>
         <source>Invalid or missing parameter: %parameter</source>
-        <translation type="unfinished"></translation>
+        <translation>Neplatný nebo chybějící parametr: %parameter</translation>
     </message>
     <message>
         <source>Use these checkboxes to select items for removal. Click the &quot;Remove selected&quot; button to  remove the selected items.</source>
-        <translation type="unfinished"></translation>
+        <translation>Použijte tyto políčka pro označení prvků k odstranění. Klikněte na tlačítko &quot;Odebrat vybrané&quot; pro odstranění označených prvků.</translation>
     </message>
     <message>
         <source>You do not have permission to remove this item.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nemáte oprávnění k odstranění tohoto prvku.</translation>
     </message>
     <message>
         <source>Remove the selected items from the list above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstranit označené prvky ze seznamu nahoře.</translation>
     </message>
     <message>
         <source>You do not have permission to remove any of the items from the list above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nemáte oprávnění k odstranění žádného prvku seznamu nahoře.</translation>
     </message>
     <message>
         <source>Automatic update</source>
-        <translation type="unfinished"></translation>
+        <translation>Automaticky ukládat priority</translation>
     </message>
     <message>
         <source>Path String</source>
-        <translation type="unfinished"></translation>
+        <translation>Řetězec umístění</translation>
     </message>
 </context>
 </TS>
